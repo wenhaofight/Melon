2b9d4fea1949f5299e2daa35b9f7cc2d04865600
WenHao <wenhaofight@qq.com>
2024-12-10 05:12:02 +0800

    change melon.conf Line Separator:\n

--- main.c
+++ main.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
-#include "mln_conf.h"
+#include "mln_framework.h"
+#include "mln_log.h"
+#include "mln_event.h"
 
-int main(int argc, char *argv[])
-{
-    mln_conf_t *cf;
-    mln_conf_domain_t *cd;
-    mln_conf_cmd_t *cc;
-    mln_conf_item_t *ci;
+char text[1024];
 
-    if (mln_conf_load() < 0) {
-        fprintf(stderr, "Load configuration failed.\n");
-        return -1;
-    }
+static int global_init(void);
 
-    cf = mln_conf();
-    cd = cf->search(cf, "main");
-    cc = cd->search(cd, "framework");
-    if (cc == NULL) {
-        fprintf(stderr, "framework not found.\n");
-        return -1;
-    }
-    ci = cc->search(cc, 1);
-    if (ci->type == CONF_BOOL && !ci->val.b) {
-        printf("framework off\n");
-    } else if (ci->type == CONF_STR) {
-        printf("framework %s\n", ci->val.s->data);
-    } else {
-        fprintf(stderr, "Invalid framework value.\n");
-    }
-    mln_conf_free();
+static void worker_process(mln_event_t *ev);
+
+static void print_handler(mln_event_t *ev, void *data);
+
+int main(int argc, char *argv[]) {
+    struct mln_framework_attr cattr;
+    cattr.argc = argc;
+    cattr.argv = argv;
+    cattr.global_init = global_init;
+    cattr.main_thread = NULL;
+    cattr.master_process = NULL;
+    cattr.worker_process = worker_process;
+    return mln_framework_init(&cattr);
+}
+
+static int global_init(void) {
+    // global variable init function
+    int n = snprintf(text, sizeof(text) - 1, "hello world\n");
+    text[n] = 0;
     return 0;
 }
 
+static void worker_process(mln_event_t *ev) {
+    // we can set event handler here
+    // let's set a timer
+    mln_event_timer_set(ev, 1000, text, print_handler);
+}
+
+static void print_handler(mln_event_t *ev, void *data) {
+    mln_log(debug, "%s\n", (char *)data);
+    mln_event_timer_set(ev, 1000, data, print_handler);
+}
